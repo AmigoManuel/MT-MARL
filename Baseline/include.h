14a3c9234ef98f8eb1cdab23e475a1e879867883
AmigoManuel <diazulloamanuel@gmail.com>
2021-05-12 20:58:42 -0400

    Ajuste sobre mapa 7 de baseline

--- Baseline/include.h
+++ Baseline/include.h
@@ -36,8 +36,8 @@
 //#define MAZEWIDTH  252
 //#define MAZEHEIGHT  163
 
-#define MAZEWIDTH  32
-#define MAZEHEIGHT  32
+#define MAZEWIDTH  34
+#define MAZEHEIGHT  34
 
 
 #define MAZEDENSITY 0.2     /* percentage of blocked cells if RANDOMMAZE is defined       */
