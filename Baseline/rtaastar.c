3fab7faa4b1af4f2574766303f6dd0c4ef696a3e
AmigoManuel <diazulloamanuel@gmail.com>
2021-07-27 00:40:05 -0400

    mediciones lookahead baseline

--- Baseline/rtaastar.c
+++ Baseline/rtaastar.c
@@ -593,7 +593,7 @@ void call_rtaastar()
     int lookahead;
     int prunning, i;
     //int look[9] = {1, 8, 16, 32, 64, 128, 256, 512, 1024};
-    int look[1] = {3};
+    int look[1] = {30};
 
     for (prunning = 0; prunning < 1; prunning++)
     {
